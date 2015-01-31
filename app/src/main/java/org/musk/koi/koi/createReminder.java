e0cfab06f369d5d7f1bb3bc6ba8a51efacf492cc
roohy <shemirani.r@gmail.com>
2015-01-31 15:04:42 -0800

    behtarinnnn commit... bara hamatoon shashidammmm

--- app/src/main/java/org/musk/koi/koi/createReminder.java
+++ app/src/main/java/org/musk/koi/koi/createReminder.java
@@ -1,22 +1,55 @@
 package org.musk.koi.koi;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardExpand;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardListView;
+import it.sephiroth.android.library.floatingmenu.FloatingActionItem;
+import it.sephiroth.android.library.floatingmenu.FloatingActionMenu;
 
 
 public class createReminder extends ActionBarActivity {
@@ -41,6 +74,7 @@ public class createReminder extends ActionBarActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new SendAsyncTask().execute("");
 
             }
         });
@@ -82,7 +116,117 @@ public class createReminder extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class SendAsyncTask extends AsyncTask<String, String, Void> {
+
+        private ProgressDialog progressDialog = new ProgressDialog(createReminder.this);
+        InputStream inputStream = null;
+        String result = "";
+
+        protected void onPreExecute() {
+            progressDialog.setMessage("Downloading your data...");
+            progressDialog.show();
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                public void onCancel(DialogInterface arg0) {
+                    SendAsyncTask.this.cancel(true);
+                }
+            });
+
+        }
+
+
+        @Override
+        protected Void doInBackground(String... params) {
+
+
+            EditText titleText = (EditText)createReminder.this.findViewById(R.id.titleText);
+            EditText noteText = (EditText)createReminder.this.findViewById(R.id.noteText);
+            DatePicker dateInput = (DatePicker)createReminder.this.findViewById(R.id.dateInput);
+            TimePicker timeInput = (TimePicker)createReminder.this.findViewById(R.id.timeInput);
+            AutoCompleteTextView placeInput = (AutoCompleteTextView)createReminder.this.findViewById(R.id.placeInput);
+
+            Log.e("Create Reminder","I am here second part of do in background");
+            String url_select = "http://koi.cloudapp.net/api/Authentication/AddReminder?userid=1&title="
+                    + URLEncoder.encode(titleText.getText().toString())+"&note="
+                    +URLEncoder.encode(noteText.getText().toString())+"&date="
+                    +URLEncoder.encode(dateInput.getDayOfMonth()+"-"+dateInput.getDayOfMonth()+"-"+dateInput.getYear())+"&time="
+                    +URLEncoder.encode(timeInput.getCurrentHour()+":"+timeInput.getCurrentMinute())+"&location="
+                    +URLEncoder.encode(placeInput.getText().toString());
+
+            ArrayList<NameValuePair> param = new ArrayList<NameValuePair>();
+
+            try {
+                // Set up HTTP post
+
+                // HttpClient is more then less deprecated. Need to change to URLConnection
+                HttpClient httpClient = new DefaultHttpClient();
+
+                HttpGet httpGet = new HttpGet(url_select);
+//                httpGet.setEntity(new UrlEncodedFormEntity(param));
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+
+                // Read content & Log
+                inputStream = httpEntity.getContent();
+            } catch (UnsupportedEncodingException e1) {
+                Log.e("UnsupportedEncodingException", e1.toString());
+                e1.printStackTrace();
+            } catch (ClientProtocolException e2) {
+                Log.e("ClientProtocolException", e2.toString());
+                e2.printStackTrace();
+            } catch (IllegalStateException e3) {
+                Log.e("IllegalStateException", e3.toString());
+                e3.printStackTrace();
+            } catch (IOException e4) {
+                Log.e("IOException", e4.toString());
+                e4.printStackTrace();
+            }
+            // Convert response to string using String Builder
+            try {
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"), 8);
+                StringBuilder sBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = bReader.readLine()) != null) {
+                    sBuilder.append(line + "\n");
+                }
+
+                inputStream.close();
+                result = sBuilder.toString();
+
+            } catch (Exception e) {
+                Log.e("StringBuilding & BufferedReader", "Error converting result " + e.toString());
+            }
+            return null;
+        }
+
+
+        protected void onPostExecute(Void v) {
+            //parse JSON data
+            try {
+//                Log.e("roohy","hahahahah");
+                ArrayList<Card> cards = new ArrayList<Card>();
+                JSONArray jArray = new JSONArray(result);
+                for(int i=0; i < jArray.length(); i++) {
+
+
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    String name = jObject.getString("Title");
+                    String tab1_text = jObject.getString("Note");
+//                    int active = jObject.getInt("active");
+                } // End Loop
+
+
+                this.progressDialog.dismiss();
+            } catch (JSONException e) {
+                Log.e("JSONException", "Error: " + e.toString());
+            } // catch (JSONException e)
+        }
+    }
+
 }
+
 
 
 class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
